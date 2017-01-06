34203c97d3a11c3672002ddd08043829ed812769
jmarkos <jkmarcosb@gmail.com>
2017-01-06 17:31:26 -0600

    changing home layout

--- template/home.php
+++ template/home.php
@@ -11,128 +11,109 @@ defined('_JEXEC') or die('Restricted access');
 
 ?>
 <!DOCTYPE html>
-	<html>
-	<head>
-		<!--
-            ##########################################
-            ## redWEB ApS                     		##
-            ## Blangstedgaardsvej 1                 ##
-            ## 5220 Odense SØ                       ##
-            ## Danmark                              ##
-            ## email@redweb.dk             			##
-            ## http://www.redweb.dk          		##
-            ## Dato: 2016-12-24                  ##
-            ##########################################
-        -->
+<html>
 
-		<w:head />
-	</head>
-	<body <?php if ($bodyclass != "") : ?> class="<?php echo $bodyclass ?>"<?php endif; ?>>
+<head>
+  <!--
+          ##########################################
+          ## redWEB ApS                     		##
+          ## Blangstedgaardsvej 1                 ##
+          ## 5220 Odense SØ                       ##
+          ## Danmark                              ##
+          ## email@redweb.dk             			##
+          ## http://www.redweb.dk          		##
+          ## Dato: 2016-12-24                  ##
+          ##########################################
+      -->
+
+  <w:head />
+</head>
+
+<body <?php if ($bodyclass !="" ) : ?> class="
+	<?php echo $bodyclass ?>"
+	<?php endif; ?>>
 	<?php if ($this->countModules('toolbar')) : ?>
-		<!-- menu -->
-		<w:nav containerClass="container" rowClass="row" wrapClass="navbar-fixed-top navbar-inverse" type="toolbar" name="toolbar" />
+	<!-- menu -->
+	<w:nav containerClass="container" rowClass="row" wrapClass="navbar-fixed-top navbar-inverse" type="toolbar" name="toolbar" />
 	<?php endif; ?>
-
-	<!-- particles.js container --> <div id="particles-js"></div> <!-- stats - count particles --> <div class="count-particles"> <span class="js-count-particles"></span></div>	
-
 	<div class="container">
-		<!-- header -->
 		<header>
-			<div class="row">
-			</div>
 		</header>
-		<?php if ($this->countModules('menu')) : ?>
-			<!-- menu -->
-			<w:nav name="menu" />
+	<?php if ($this->countModules('menu')) : ?>
+	<!-- menu -->
+		<w:nav name="menu" />
 		<?php endif; ?>
-
 		<?php if ($this->countModules('featured')) : ?>
-			<!-- featured -->
-			<div id="featured">
-				<w:module type="none" name="featured" chrome="whtml" />
-			</div>
+		<!-- featured -->
+		<div id="featured">
+			<w:module type="none" name="featured" chrome="whtml" />
+		</div>
 		<?php endif; ?>
-
 		<?php if ($this->countModules('grid-top')) : ?>
-			<!-- grid-top -->
-			<div id="grid-top">
-				<w:module type="row" name="grid-top" chrome="wrightflexgrid" />
-			</div>
+		<!-- grid-top -->
+		<div id="grid-top">
+			<w:module type="row" name="grid-top" chrome="wrightflexgrid" />
+		</div>
 		<?php endif; ?>
 		<?php if ($this->countModules('grid-top2')) : ?>
-			<!-- grid-top2 -->
-			<div id="grid-top2">
-				<w:module type="row" name="grid-top2" chrome="wrightflexgrid" />
-			</div>
-		<?php endif; ?>
-		<div id="main-content" class="row">
-			<!-- sidebar1 -->
-			<aside id="sidebar1">
-				<w:module name="sidebar1" chrome="whtml" />
-			</aside>
-			<!-- main -->
-			<section id="main">
-				<?php if ($this->countModules('above-content')) : ?>
-					<!-- above-content -->
-					<div id="above-content">
-						<w:module type="none" name="above-content" chrome="whtml" />
-					</div>
+      <!-- grid-top2 -->
+				<div id="grid-top2">
+					<w:module type="row" name="grid-top2" chrome="wrightflexgrid" />
+				</div>
 				<?php endif; ?>
-				<?php if ($this->countModules('breadcrumbs')) : ?>
-					<!-- breadcrumbs -->
+				<div id="main-content" class="row">
+					<aside id="sidebar1">
+						<w:module name="sidebar1" chrome="whtml" />
+					</aside>
+					<!-- main -->
+					<section id="main">
+					<?php if ($this->countModules('above-content')) : ?>
+					<!-- above-content -->
+						<div id="above-content">
+					<w:module type="none" name="above-content" chrome="whtml" />
+					</div>
+					<?php endif; ?>
+					<?php if ($this->countModules('breadcrumbs')) : ?>
 					<div id="breadcrumbs">
 						<w:module type="single" name="breadcrumbs" chrome="none" />
 					</div>
-				<?php endif; ?>
-				<!-- component -->
-				<w:content />
-				<?php if ($this->countModules('below-content')) : ?>
-					<!-- below-content -->
+					<?php endif; ?>
+					<!-- component -->
+					<w:content />
+					<?php if ($this->countModules('below-content')) : ?>
 					<div id="below-content">
 						<w:module type="none" name="below-content" chrome="whtml" />
 					</div>
+					<?php endif; ?>
+					</section>
+					<aside id="sidebar2">
+						<w:module name="sidebar2" chrome="whtml" />
+					</aside>
+				</div>
+				<?php if ($this->countModules('grid-bottom')) : ?>
+				<div id="grid-bottom">
+					<w:module type="row" name="grid-bottom" chrome="wrightflexgrid" />
+				</div>
 				<?php endif; ?>
-			</section>
-			<!-- sidebar2 -->
-			<aside id="sidebar2">
-				<w:module name="sidebar2" chrome="whtml" />
-			</aside>
-		</div>
-		<?php if ($this->countModules('grid-bottom')) : ?>
-			<!-- grid-bottom -->
-			<div id="grid-bottom">
-				<w:module type="row" name="grid-bottom" chrome="wrightflexgrid" />
-			</div>
-		<?php endif; ?>
-		<?php if ($this->countModules('grid-bottom2')) : ?>
-			<!-- grid-bottom2 -->
-			<div id="grid-bottom2">
-				<w:module type="row" name="grid-bottom2" chrome="wrightflexgrid" />
-			</div>
-		<?php endif; ?>
-		<?php if ($this->countModules('bottom-menu')) : ?>
-			<!-- bottom-menu -->
-			<w:nav containerClass="container" rowClass="row" name="bottom-menu" />
-		<?php endif; ?>
-	</div>
-
-	<!-- footer -->
-	<div class="wrapper-footer">
-		<footer id="footer" <?php if ($this->params->get('stickyFooter', 1)) : ?> class="sticky"<?php endif;?>>
-			<div class="container">
-				<?php if ($this->countModules('footer')) : ?>
-					<w:module type="row" name="footer" chrome="wrightflexgrid" />
+				<?php if ($this->countModules('grid-bottom2')) : ?>
+				<div id="grid-bottom2">
+					<w:module type="row" name="grid-bottom2" chrome="wrightflexgrid" />
+				</div>
 				<?php endif; ?>
-
-			</div>
-		</footer>
-	</div>
-
-	<w:footer />
-
-	</body>
-	<script type="text/javascript" src="<?php echo $this->baseurl . '/templates/jmarkos013/js/particles.js'; ?>">
-	</script>
-	<script type="text/javascript" src="<?php echo $this->baseurl . '/templates/jmarkos013/js/call.js'; ?>">
-	</script>
+				<?php if ($this->countModules('bottom-menu')) : ?>
+				<w:nav containerClass="container" rowClass="row" name="bottom-menu" />
+				<?php endif; ?>
+      </div>
+      <div class="wrapper-footer">
+        <footer id="footer" <?php if ($this->params->get('stickyFooter', 1)) : ?> class="sticky"
+          <?php endif;?>>
+          <div class="container">
+						<?php if ($this->countModules('footer')) : ?>
+						<w:module type="row" name="footer" chrome="wrightflexgrid" />
+						<?php endif; ?>
+          </div>
+        </footer>
+      </div>
+			<w:footer />
+  </body>
 </html>
